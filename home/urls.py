dfce57e46ebf274b19aa4f0b6c21276f4caa3719
KashishRathod17 <rathodkashish03@gmail.com>
2025-02-01 17:53:40 +0530

    Updated

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('index',views.index,name='index'),
     path('home',views.home,name='home'),
     path('about',views.about,name='about'),
     path('skills',views.skills,name='skills'),
